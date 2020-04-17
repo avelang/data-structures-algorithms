56c3218cb2baa362988d26139d69e721aaf4b6bf
Abey Velangadan <abey.velangadan@gmail.com>
2020-04-17 16:59:37 +0530

    renamed project. added numeric algorithms.

---
+++ algorithms.Tests/NumericAlgorithmsTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms.Tests
+{
+    [TestClass()]
+    public class NumericAlgorithmsTests
+    {
+        [TestMethod()]
+        public void reverse_test_1()
+        {
+            int input = 10;
+            NumericAlgorithms algos = new NumericAlgorithms();
+            int actual = algos.Reverse(input);
+            int expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void reverse_test_2()
+        {
+            int input = 1001;
+            NumericAlgorithms algos = new NumericAlgorithms();
+            int actual = algos.Reverse(input);
+            int expected = 1001;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void reverse_test_3()
+        {
+            int input = 1234;
+            NumericAlgorithms algos = new NumericAlgorithms();
+            int actual = algos.Reverse(input);
+            int expected = 4321;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void reverse_test_4()
+        {
+            int input = 670045235;
+            NumericAlgorithms algos = new NumericAlgorithms();
+            int actual = algos.Reverse(input);
+            int expected = 532540076;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void reverse_test_5()
+        {
+            int input = 0;
+            NumericAlgorithms algos = new NumericAlgorithms();
+            int actual = algos.Reverse(input);
+            int expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ algorithms/NumericAlgorithmscs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms
+{
+    public class NumericAlgorithms
+    {
+        public int reverse(int num)
+        {
+            int output = 0;
+            while(num>0)
+            {
+                output = output*10 + num % 10;
+                num = num / 10;
+            }
+            return output;
+        }
+    }
+}
