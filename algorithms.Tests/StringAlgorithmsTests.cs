9dfdcdd05c6f688a135726f7f8f82f9ce7b91f41
Abey Velangadan <abey.velangadan@gmail.com>
2020-04-22 21:17:51 +0530

    new files added

---
+++ algorithms.Tests/StringAlgorithmsTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms.Tests
+{
+    [TestClass()]
+    public class StringAlgorithmsTests
+    {
+        public StringAlgorithms strAlgo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            strAlgo = new StringAlgorithms();
+        }
+        [TestMethod()]
+        public void isValidParanthesis_Test_1()
+        {
+            string inputString = "{}{}[]()[]{}{}";
+            Assert.IsTrue(strAlgo.isValidParanthesis(inputString));
+        }
+
+        [TestMethod()]
+        public void isValidParanthesis_Test_2()
+        {
+            string inputString = "{[()()][]}";
+            Assert.IsTrue(strAlgo.isValidParanthesis(inputString));
+        }
+
+        [TestMethod()]
+        public void isValidParanthesis_Test_3()
+        {
+            string inputString = "{}{}[]()[]{}}{}";
+            Assert.IsFalse(strAlgo.isValidParanthesis(inputString));
+        }
+
+        [TestMethod()]
+        public void isValidParanthesis_Test_4()
+        {
+            string inputString = "{[()()][]}}";
+            Assert.IsFalse(strAlgo.isValidParanthesis(inputString));
+        }
+        [TestMethod()]
+        public void isValidParanthesis_Test_5()
+        {
+            string inputString = "{}{}[]()[]{}{{}";
+            Assert.IsFalse(strAlgo.isValidParanthesis(inputString));
+        }
+
+        [TestMethod()]
+        public void isValidParanthesis_Test_6()
+        {
+            string inputString = "{[()()][]}[";
+            Assert.IsFalse(strAlgo.isValidParanthesis(inputString));
+        }
+    }
+}
---
+++ algorithms/StringAlgorithms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms
+{
+    public class StringAlgorithms
+    {
+        public bool isValidParanthesis(string input)
+        {
+            HashSet<char> openChars = new HashSet<char> { '{', '[', '(' };
+            Dictionary<char, char> closeChars = new Dictionary<char, char>();
+            closeChars.Add('}', '{');
+            closeChars.Add(']', '[');
+            closeChars.Add(')', '(');
+            Stack<char> inpStr = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(openChars.Contains(input[i]))
+                {
+                    inpStr.Push(input[i]);
+                }
+                else if(closeChars.ContainsKey(input[i]))
+                {
+                    if (inpStr.Count != 0)
+                    {
+                        if (inpStr.Peek() == closeChars[input[i]])
+                        {
+                            inpStr.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return (inpStr.Count == 0) ? true : false;
+        }
+    }
+}
