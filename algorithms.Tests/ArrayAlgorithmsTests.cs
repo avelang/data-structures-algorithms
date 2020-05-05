2e9708ff53e4d4ec8e0fb38f778bc2d560078ba0
Abey Velangadan <abey.velangadan@gmail.com>
2020-05-05 19:39:40 +0530

    added code & test cases for merge algorithm

---
+++ algorithms.Tests/ArrayAlgorithmsTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms.Tests
+{
+    [TestClass()]
+    public class ArrayAlgorithmsTests
+    {
+        public ArrayAlgorithms arrayAlgo;
+
+        [TestInitialize]
+        public void Init()
+        {
+            arrayAlgo = new ArrayAlgorithms();
+        }
+        [TestMethod()]
+        public void Merge_Test_1()
+        {
+            int[] a = { 1, 2 };
+            int[] b = { 3, 4 };
+            int[] expected = { 1, 2, 3, 4 };
+            int[] actual = arrayAlgo.Merge(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Merge_Test_2()
+        {
+            int[] a = { 1, 3 };
+            int[] b = { 2, 4 };
+            int[] expected = { 1, 2, 3, 4 };
+            int[] actual = arrayAlgo.Merge(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Merge_Test_3()
+        {
+            int[] a = { 1, 3, 4 };
+            int[] b = { 2, 4 };
+            int[] expected = { 1, 2, 3, 4, 4 };
+            int[] actual = arrayAlgo.Merge(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Merge_Test_4()
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] b = { 6, 7, 8, 9, 10 };
+            int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] actual = arrayAlgo.Merge(a, b);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ algorithms/ArrayAlgorithms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithms
+{
+    public class ArrayAlgorithms
+    {
+        // Problem : Merge two sorted arrays into one single sorted array.
+        public int[] Merge(int[] a, int[] b)
+        {
+            int totCount = a.Length + b.Length;
+            int[] c = new int[totCount];
+            int count = 0;
+            if(a.Length==0 && b.Length>0)
+            {
+                return b;
+            }
+            else if(a.Length > 0 && b.Length == 0)
+            {
+                return a;
+            }
+            else if(a.Length == 0 && b.Length == 0)
+            {
+                return new int[] { };
+            }
+            else
+            {
+                int a_index = 0;
+                int b_index = 0;
+                int a_current = a[a_index];
+                int b_current = b[b_index];
+                while (count < totCount)
+                {
+                    if(a_current < b_current)
+                    {
+                        c[count] = a[a_index];
+                        a_current = (a_index == a.Length - 1) ? int.MaxValue : a[++a_index];
+                    }
+                    else
+                    {
+                        c[count] = b[b_index];
+                        b_current = (b_index == b.Length - 1) ? int.MaxValue : b[++b_index];
+                    }
+                    count++;
+                }
+                return c;
+            }
+        }
+    }
+}
